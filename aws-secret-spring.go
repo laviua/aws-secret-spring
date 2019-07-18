a1807dcee6f3c6c1b57e434ac253d47427696e5a
Oleksandr Loushkin <o.loushkin@levi9.com>
2019-07-18 17:11:26 +0300

    Add ability to filter secrets by app tag

--- aws-secret-spring.go
+++ aws-secret-spring.go
@@ -5,10 +5,13 @@ import (
 	"fmt"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/secretsmanager"
+	"os"
 	"strings"
 )
 
 func main() {
+	appname := os.Args[1]
+
 	s := session.Must(session.NewSession())
 	sm := secretsmanager.New(s)
 	result, err := sm.ListSecrets(&secretsmanager.ListSecretsInput{})
@@ -16,25 +19,38 @@ func main() {
 		panic(err.Error())
 	}
 	configtext := "{"
-	for _, element := range result.SecretList {
-		sn := element.Name
-		output, err := sm.GetSecretValue(&secretsmanager.GetSecretValueInput{SecretId: sn})
-		if err != nil {
-			panic(err.Error())
-		}
+	for _, secretElement := range result.SecretList {
 
-		var objmap map[string]interface{}
-		err = json.Unmarshal([]byte(*output.SecretString), &objmap)
+		// if app tag matched - go ahead
+		if matchApplication(secretElement, appname) {
 
-		if err != nil {
-			panic(err.Error())
-		}
+			output, err := sm.GetSecretValue(&secretsmanager.GetSecretValueInput{SecretId: secretElement.Name})
+			if err != nil {
+				panic(err.Error())
+			}
 
-		for key, value := range objmap {
-			configtext += "\"" + key + "\"" + ":" + "\"" + value.(string) + "\"" + ","
+			var objmap map[string]interface{}
+			err = json.Unmarshal([]byte(*output.SecretString), &objmap)
+
+			if err != nil {
+				panic(err.Error())
+			}
+
+			for key, value := range objmap {
+				configtext += "\"" + key + "\"" + ":" + "\"" + value.(string) + "\"" + ","
+			}
 		}
 	}
 	configtext = strings.TrimSuffix(configtext, ",")
 	configtext += "}"
 	fmt.Print(configtext)
+}
+
+func matchApplication(element *secretsmanager.SecretListEntry, appname string) bool {
+	for _, tag := range element.Tags {
+		if *tag.Key == "app" && *tag.Value != appname {
+			return false
+		}
+	}
+	return true
 }
